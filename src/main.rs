d19e9a0e6b5bf918ab9690087e8eda5a6bb03fff
Toni Poščić <tposcic@gmail.com>
2024-08-05 23:52:19 +0200

    added connections throttleing, added .env file for settings like db connection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use std::
 {
     io::{prelude::*, BufReader},//buffer reader
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    time::{Duration, Instant}
 };
 
 use mysql::prelude::*;
 use mysql::*;
 use serde::Serialize;
+use dotenv::dotenv;
+use std::env;
+
+const MAX_CONNECTIONS: usize = 60; // Maximum number of connections allowed per client in the time defined in TIME_INTERVAL
+const TIME_INTERVAL: Duration = Duration::from_secs(60); // Time interval in seconds
 
 #[derive(Debug, PartialEq, Eq, Serialize)]
 struct Player {
@@ -20,25 +28,69 @@ struct PlayersResponse {
     players: Vec<Player>,
 }
 
+struct Throttle {
+    count: usize,
+    last_reset: Instant,
+}
+
+impl Throttle {
+    fn new() -> Self {
+        Throttle {
+            count: 0,
+            last_reset: Instant::now(),
+        }
+    }
+
+    fn check_and_increment(&mut self) -> bool {
+        if self.last_reset.elapsed() > TIME_INTERVAL {
+            self.count = 0;
+            self.last_reset = Instant::now();
+        }
+
+        if self.count < MAX_CONNECTIONS {
+            self.count += 1;
+            true
+        } else {
+            false
+        }
+    }
+}
+
 fn main() 
 {
+    dotenv().ok();
+    let database_url = env::var("DB").expect("DB (database url) must be set");
+
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let throttle_map = Arc::new(Mutex::new(HashMap::new()));
 
     for stream in listener.incoming()
     {
         let stream = stream.unwrap();
+        let throttle_map = Arc::clone(&throttle_map);
+        
+        let database_url = database_url.clone(); // Clone the database_url for each thread
 
-        handle_connection(stream);
+        std::thread::spawn(move || {
+            let peer_addr = stream.peer_addr().unwrap().ip();
+            let mut throttle_map = throttle_map.lock().unwrap();
+            let throttle = throttle_map.entry(peer_addr).or_insert_with(Throttle::new);
+
+            if throttle.check_and_increment() {
+                handle_connection(stream, &database_url);
+            } else {
+                send_too_many_requests_response(&stream);
+            }
+        });
     }
 }
 
-fn handle_connection(mut stream: TcpStream)
+fn handle_connection(mut stream: TcpStream, database_url: &str)
 {
     let mut buffer_reader = BufReader::new(&mut stream);
     let request_line = buffer_reader.by_ref().lines().next().unwrap().unwrap();
 
-    let url = "mysql://root:@127.0.0.1:3306/game_de";
-    let pool = Pool::new(url).unwrap();
+    let pool = Pool::new(database_url).unwrap();
     let mut conn = pool.get_conn().unwrap();
 
     if request_line == "GET /players/top HTTP/1.1" {
@@ -145,6 +197,12 @@ fn send_not_found_response(stream: &mut TcpStream, status_line: &str)
     );
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn send_too_many_requests_response(mut stream: &TcpStream) {
+    let response = "HTTP/1.1 429 Too Many Requests\r\nContent-Length: 0\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 // fn send_bad_request_response(stream: &mut TcpStream, status: &str) {
