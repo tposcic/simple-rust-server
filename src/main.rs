8571f5b6e050983f4e6f922626019e4bfb3c2201
Toni Poščić <tposcic@gmail.com>
2024-08-06 02:35:38 +0200

    check authorization tokens for database operations

--- src/main.rs
+++ src/main.rs
@@ -88,36 +88,63 @@ fn main()
 fn handle_connection(mut stream: TcpStream, database_url: &str)
 {
     let mut buffer_reader = BufReader::new(&mut stream);
-    let request_line = buffer_reader.by_ref().lines().next().unwrap().unwrap();
+
+    let request_line = match buffer_reader.by_ref().lines().next() {
+        Some(Ok(line)) => line,
+        Some(Err(e)) => {
+            eprintln!("Error reading request line: {}", e);
+            return;
+        }
+        None => {
+            eprintln!("No request line found");
+            return;
+        }
+    };
+
+    let mut headers = HashMap::new();
+    let mut token = "";
+    let mut content_length = 0;
+
+    for line in buffer_reader.by_ref().lines() {
+        let line = match line {
+            Ok(line) => line,
+            Err(e) => {
+                eprintln!("Error reading header line: {}", e);
+                return;
+            }
+        };
+        
+        if line.is_empty() {
+            break;
+        }
+        
+        let parts: Vec<&str> = line.splitn(2, ": ").collect();
+        
+        if line.starts_with("Content-Length:") {
+            let parts: Vec<&str> = line.split(':').collect();
+            content_length = parts[1].trim().parse::<usize>().unwrap();
+        }
+        
+        if parts.len() == 2 {
+            headers.insert(parts[0].to_string(), parts[1].to_string());
+        }
+    }
+
+    if let Some(auth_header) = headers.get("Authorization") {
+        token = auth_header.trim_start_matches("Bearer ");
+    }
 
     let pool = Pool::new(database_url).unwrap();
     let mut conn = pool.get_conn().unwrap();
 
     if request_line == "GET /players/top HTTP/1.1" {
-        match fetch_top_players(&mut conn) {
+        match fetch_top_players(&mut conn,token) {
             Ok(players) => send_players_response(&mut stream, "HTTP/1.1 200 OK", players),
             Err(e) => eprintln!("Error fetching players: {}", e),
         }
     }
     else if request_line == "POST /player HTTP/1.1"
-    {
-        let mut headers = String::new();
-        let mut content_length = 0;
-    
-        for line in buffer_reader.by_ref().lines() {
-            let line = line.unwrap();
-            if line.is_empty() {
-                break;
-            }
-            headers.push_str(&line);
-            headers.push_str("\r\n");
-
-            if line.starts_with("Content-Length:") {
-                let parts: Vec<&str> = line.split(':').collect();
-                content_length = parts[1].trim().parse::<usize>().unwrap();
-            }
-        }
-    
+    {    
         let mut body = vec![0; content_length];
         buffer_reader.read_exact(&mut body).unwrap();
         let body_str = String::from_utf8(body).unwrap();
@@ -127,7 +154,7 @@ fn handle_connection(mut stream: TcpStream, database_url: &str)
         let username = player_data["username"].as_str().unwrap();
         let score = player_data["score"].as_i64().unwrap() as i32;
     
-        match insert_player(&mut conn, username, score.into()) {
+        match insert_player(&mut conn, username, score.into(),token) {
             Ok(player) => send_players_response(&mut stream, "HTTP/1.1 200 OK", vec![player]),
             Err(e) => eprintln!("Error inserting player: {}", e),
         }
@@ -138,41 +165,64 @@ fn handle_connection(mut stream: TcpStream, database_url: &str)
     };
 }
 
-fn fetch_top_players(conn: &mut PooledConn) -> std::result::Result<Vec<Player>, mysql::Error> 
+fn fetch_top_players(conn: &mut PooledConn, token: &str) -> std::result::Result<Vec<Player>, mysql::Error> 
 {
-    conn.query_map(
-        "SELECT id, `username`, score FROM top_players",
-        |(id, username, score)| {
-            Player { id, username, score }
+    match check_token(conn, token)? {
+        true => {
+            conn.query_map(
+                "SELECT id, `username`, score FROM top_players",
+                |(id, username, score)| {
+                    Player { id, username, score }
+                }
+            )
+        },
+        false => {
+            Err(mysql::Error::MySqlError(mysql::MySqlError {
+                code: 400,
+                message: "Bad Request".to_string(),
+                state: "HY000".to_string(), // Adding the state field
+            }))        
         }
-    )
+    }
+
+
 }
 
-fn insert_player(conn: &mut PooledConn, username: &str, score: i64) -> std::result::Result<Player, mysql::Error> {
-    conn.exec_drop(
-        "DELETE FROM top_players WHERE username = :username",
-        params! {
-            "username" => username,
-            "score" => score,
+fn insert_player(conn: &mut PooledConn, username: &str, score: i64, token: &str) -> std::result::Result<Player, mysql::Error> {
+    match check_token(conn, token)? {
+        true => {
+            conn.exec_drop(
+                "DELETE FROM top_players WHERE username = :username",
+                params! {
+                    "username" => username,
+                },
+            )?;
+
+            conn.exec_drop(
+                "INSERT INTO top_players (username, score) VALUES (:username, :score)",
+                params! {
+                    "username" => username,
+                    "score" => score,
+                },
+            )?;
+
+            let player_id = conn.last_insert_id();
+            let player = Player {
+                id: player_id as i64,
+                username: username.to_string(),
+                score,
+            };
+
+            Ok(player)
         },
-    )?;
-
-    conn.exec_drop(
-        "INSERT INTO top_players (username, score) VALUES (:username, :score)",
-        params! {
-            "username" => username,
-            "score" => score,
-        },
-    )?;
-
-    let player_id = conn.last_insert_id();
-    let player = Player {
-        id: player_id as i64,
-        username: username.to_string(),
-        score,
-    };
-
-    Ok(player)
+        false => {
+            Err(mysql::Error::MySqlError(mysql::MySqlError {
+                code: 400,
+                message: "Bad Request".to_string(),
+                state: "HY000".to_string(), // Adding the state field
+            }))        
+        }
+    }
 }
 
 fn send_players_response(stream: &mut TcpStream, status_line: &str, players: Vec<Player>) 
@@ -205,11 +255,15 @@ fn send_too_many_requests_response(mut stream: &TcpStream) {
     stream.flush().unwrap();
 }
 
-// fn send_bad_request_response(stream: &mut TcpStream, status: &str) {
-//     let response = format!(
-//         "{}\r\nContent-Length: 0\r\n\r\n",
-//         status
-//     );
-//     stream.write_all(response.as_bytes()).unwrap();
-//     stream.flush().unwrap();
-// }
+fn check_token(conn: &mut PooledConn, token: &str) -> Result<bool> {
+    match conn.exec_first::<Option<String>, _, _>(
+        "SELECT token FROM tokens WHERE token = :token",
+        params! {
+            "token" => token,
+        },
+    ) {
+        Ok(Some(_)) => Ok(true),  // Token found
+        Ok(None) => Ok(false),    // Token not found
+        Err(e) => Err(e),         // Error occurred
+    }
+}
